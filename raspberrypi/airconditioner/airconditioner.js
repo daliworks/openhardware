977aa0b94d24418a7404d4f82d7006f07f621b01
Younghyun Jo <younghyun.jo@daliworks.net>
2016-03-24 20:46:24 +0900

    [pi] airconditioner device agent added

---
+++ raspberrypi/airconditioner/airconditioner.js
@@ -0,0 +1,38 @@
+var exec = require('child_process').exec,
+    locks = require('locks');
+
+var mutex = locks.createMutex();
+
+//  irsend SEND_ONCE LGE_6711A20015N UN-JEON/JEONG-JI_26
+exports.on = function (cb) {
+  var cmd = 'irsend SEND_ONCE LGE_6711A20015N UN-JEON/JEONG-JI_22';
+  mutex.lock(function () {
+    exec(cmd, function (err, stdout, stderr) {
+      mutex.unlock();
+      if (err) {
+        return cb && cb(err);
+      }
+
+      return cb && cb(null);
+    });
+  });
+};
+
+//irsend SEND_ONCE LGE_6711A20015N UN-JEON/JEONG-JI_OFF
+exports.off = function (cb) {
+  var cmd = 'irsend SEND_ONCE LGE_6711A20015N UN-JEON/JEONG-JI_OFF';
+  mutex.lock(function () {
+    exec(cmd, function (err, stdout, stderr) {
+      mutex.unlock();
+      if (err) {
+        return cb && cb(err);
+      }
+
+      return cb && cb(null);
+    });
+  });
+};
+
+if (require.main == module) {
+  exports.on();
+}
---
+++ raspberrypi/airconditioner/app.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var jsonrpc = require('jsonrpc-tcp'),
+    winston = require('winston'),
+    _ = require('lodash');
+
+var airconditioner = require('./airconditioner');
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({label: 'DEVICE_AGENT'}),
+    new (winston.transports.File)({
+      label: 'DEVICE_AGENT', 
+      filename: 'log/device_agent.log', 
+      json: false,
+      handleExceptions: true,
+      humanReadableUnhandledException: true,
+      level: 'debug'}),
+  ]
+});
+
+var tubeServer = require('./tube_server');
+
+var deviceAgent ={};
+deviceAgent.sensors = [
+  {
+    name: 'Airconditioner',
+    type: 'powerSwitch',
+    actuating: airconditionerActuating,
+  }
+];
+
+function airconditionerActuating(cmd, options, cb) {
+  function _callback(err) {
+    if (err) {
+      logger.error('relay actuating failed');
+      return cb && cb(new Error('relay actuating failed'));
+    }
+
+    return cb && cb(null, options || 'ok');
+  }
+
+  switch (cmd) {
+    case 'on':
+      airconditioner.on(_callback);
+    case 'off':
+      airconditioner.off(_callback);
+    default:
+      return cb && cb(new Error('unknown cmd'));
+  }
+}
+
+function discover(cb) {
+  logger.debug('request discover');
+  cb && cb(deviceAgent.sensors);
+}
+
+function actuating(name, cmd, options, cb) {
+  logger.debug('request actuating %s', name);
+
+  var sensor = _.find(deviceAgent.sensors, {'name': name});
+  if (!sensor) {
+    logger.error('invalid sensor name');
+    return cb && cb(new Error('invalid sensor name'));
+  }
+
+  sensor.actuating(cmd, options, function (err, reults) {
+    if (err) {
+      logger.error('actuating faied');
+      return cb && cb(err);
+    }
+
+    return cb && cb(null, result);
+  });
+}
+
+function sensing(name, cb) {
+  logger.debug('request sensing(%s)', name);
+
+  var sensor = _.find(deviceAgent.sensors, {'name': name});
+  if (!sensor) {
+    logger.error('invalid sensor name');
+    return cb && cb(new Error('invalid sensor name'));
+  }
+}
+
+function sensorStatus(name, cb) {
+  logger.debug('request %s status', name);
+  cb && cb('on');
+}
+
+deviceAgent.init = function () {
+  tubeServer.init(deviceAgent.sensors, discover, sensing, actuating, sensorStatus);
+};
+
+deviceAgent.init();
