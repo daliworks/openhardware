5a1686fbe0590fba541277d9a1ac28bed9e9f9d8
Jaeseok Park <jaeseok.park@daliworks.net>
2017-12-06 19:00:34 +0900

    [rasp/camera] Add deviceModelId for network discovering

--- raspberrypi/camera/app.js
+++ raspberrypi/camera/app.js
@@ -145,7 +145,7 @@ Device.prototype.discovering = function (result) {
   var devices = [];
   var self = this;
 
-  devices[this.id] = {deviceAddress: this.id};
+  devices[this.id] = {deviceAddress: this.id, deviceModelId: 'jsonrpcFullV1.0'};
   devices[this.id]['sensors'] = [];
 
   _.forEach(this.sensors, function (sensor) {
